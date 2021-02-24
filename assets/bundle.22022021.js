ec2305d108d64c5b208ac7e4e16047c917f193ab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-24 17:49:44 +0000

    Deploy noelvaranda.dev to github.com/nowy/nowy.github.io.git:gh-pages

--- assets/bundle.22022021.js
+++ assets/bundle.22022021.js
@@ -50858,12 +50858,6 @@ const createApp = async ({
     });
     appMain.innerHTML = homeHTML;
   });
-  router.on('/archive', () => {
-    toggleNetwork({
-      isOpen: true
-    });
-    appMain.innerHTML = renderArchive();
-  });
   router.on('/notes/:id', ({
     data
   }) => {
@@ -50887,7 +50881,7 @@ const createApp = async ({
 
     if (router.getCurrentLocation().route.path === '') {
       const selectedNotes = network.getSelectedNodes();
-      router.navigate(selectedNotes[0] ? `/notes/${selectedNotes[0]}` : '/archive');
+      router.navigate(selectedNotes[0] ? `/notes/${selectedNotes[0]}` : '/notes/202102241726');
       return;
     }
 
@@ -50927,30 +50921,5 @@ const createApp = async ({
 function mapBy(array, key) {
   return Object.fromEntries(array.map(entry => [entry[key], entry]));
 }
-
-const renderArchive = () => `
-<section class="content">
-  <a href="/" class="content__back" id="page-back" data-navigo>Return back home</a>
-  <h1 id="page-title">Notes Archive</h1>
-  <div class="page-content">
-    <p class="paragraph">
-      I recently started a new way of memorizing the things I learn, which is essentially to write them in my own words as soon as I read them.
-      I follow the <a href="https://en.wikipedia.org/wiki/Zettelkasten" target="_blank">Zettelkasten</a> method of taking notes. Take a peek at the notes I've kept and how they connect to each other.
-    </p>
-    <p class="paragraph">
-      My hope is that one day, eventually my zettelkasten will be big enough that it can be of use to other people. This is <strong>still very bare</strong>, I plan on adding to it as I read (and note down) more.
-    </p>
-    <p class="paragraph"></p>
-    <h3>How to use it</h3>
-    <p class="paragraph">
-      Easy. Click on the nodes on the right hand side. Colours represent their "tags", and the links recommend the next node. I plan on adding better visualisation in the future so hang on tight.
-    </p>
-    <h3>Shout out</h3>
-    <p class="paragraph">
-      All reference you see right now are from Michael Geers' <a href="https://www.manning.com/books/micro-frontends-in-action" target="_blank">Micro Frontends in Action</a>, so thank you for being my first reference guide.
-    </p>
-  </div>
-</section>
-`;
 
 export { createApp };
